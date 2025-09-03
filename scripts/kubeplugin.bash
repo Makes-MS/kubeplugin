93fb77ebb11b04d200b81646b4ecff1a681bbdf9
Maxym <MakesMS24@gmail.com>
2025-09-03 14:12:43 +0300

    Change kubeplugin file

--- scripts/kubeplugin.bash
+++ scripts/kubeplugin.bash
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-# Define command-line arguments
+# kubeplugin - bash-плагін для kubectl для виведення статистики ресурсів
+# Використання:
+# ./kubeplugin <namespace> <resource_type>
+# Приклад:
+# ./kubeplugin kube-system pods
 
-NAMESPACE=$1
-RESOURCE_TYPE=$2
+set -e
 
-if [ -z "$NAMESPACE" ] || [ -z "$RESOURCE_TYPE" ]; then
+NAMESPACE="$1"
+RESOURCE_TYPE="$2"
+
+if [[ -z "$NAMESPACE" || -z "$RESOURCE_TYPE" ]]; then
   echo "Usage: $0 <namespace> <resource_type>"
   exit 1
 fi
 
-# Retrieve resource usage statistics from Kubernetes
-kubectl top $RESOURCE_TYPE -n $NAMESPACE | tail -n +2 | while read line
-do
-  # Extract CPU and memory usage from the output
-  NAME=$(echo $line | awk '{print $1}')
-  CPU=$(echo $line | awk '{print $2}')
-  MEMORY=$(echo $line | awk '{print $3}')
+echo "Resource, Namespace, Name, CPU, Memory"
 
-  # Output the statistics to the console
-  # "Resource, Namespace, Name, CPU, Memory"
-  echo "$RESOURCE_TYPE,$NAMESPACE,$NAME,$CPU,$MEMORY"
+kubectl top "$RESOURCE_TYPE" -n "$NAMESPACE" --no-headers | while read -r line; do
+    NAME=$(echo "$line" | awk '{print $1}')
+    CPU=$(echo "$line" | awk '{print $2}')
+    MEMORY=$(echo "$line" | awk '{print $3}')
+    echo "$RESOURCE_TYPE, $NAMESPACE, $NAME, $CPU, $MEMORY"
 done
